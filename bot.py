5f39a398582d0b77a653205c2a7804f8326ce2a5
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2021-04-07 23:13:13 +0200

    print -> logging

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ class YTVideo:
         start = start if start is not None else self._url.timecode()
         # comes from https://old.reddit.com/r/youtubedl/comments/b67xh5/downloading_part_of_a_youtube_video/ejv5ye7/
         cmd = f"ffmpeg -ss {start} -i $(youtube-dl -f 22 -g {self._url.value()}) -acodec copy -vcodec copy -t {duration} {out_file} -y"
-        print(cmd)
+        logging.info(cmd)
         p = subprocess.Popen(cmd, shell=True)
         p.wait()
 
