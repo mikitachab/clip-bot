d2595d3afd7d89da248c1929112cded3d588001f
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2021-04-10 11:13:34 +0200

    add timecode class

---
+++ test_timecode.py
@@ -0,0 +1,30 @@
+import pytest
+
+from timecode import make_timecode, SecondsTimecode, FormattedTimecode, InvalidTimecodeError
+
+
+@pytest.mark.parametrize(
+    "user_input, timecode_cls, expected_seconds",
+    [
+        ("123", SecondsTimecode, 123),
+        ("234s", FormattedTimecode, 234),
+        ("30m", FormattedTimecode, 1800),
+        ("30m20s", FormattedTimecode, 1820),
+    ],
+)
+def test_make_timecode(user_input, timecode_cls, expected_seconds):
+    t = make_timecode(user_input)
+    assert isinstance(t, timecode_cls)
+    assert t.seconds == expected_seconds
+
+
+@pytest.mark.parametrize(
+    "user_input",
+    [
+        "some text",
+        "3s5h",
+    ],
+)
+def test_raise_on_invalid_timecode(user_input):
+    with pytest.raises(InvalidTimecodeError):
+        make_timecode(user_input)
---
+++ timecode.py
@@ -0,0 +1,57 @@
+import abc
+import re
+
+
+class InvalidTimecodeError(Exception):
+    pass
+
+
+class TimecodeInterface(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def seconds(self) -> int:
+        raise NotImplemented
+
+
+class SecondsTimecode(TimecodeInterface):
+    PATTERN = r"^\d+$"
+
+    def __init__(self, text: str):
+        self._seconds = int(text)
+
+    @property
+    def seconds(self) -> int:
+        return self._seconds
+
+
+class FormattedTimecode(TimecodeInterface):
+
+    PATTERN = r"^(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?$"
+
+    def __init__(self, text: str):
+        self._text = text
+        self._seconds: int = 0
+        self._minutes: int = 0
+        self._houres: int = 0
+        self._extract_timecode()
+
+    def _extract_timecode(self):
+        h, m, s = re.match(self.PATTERN, self._text).groups()
+        if h:
+            self._houres = int(h)
+        if m:
+            self._minutes = int(m)
+        if s:
+            self._seconds = int(s)
+
+    @property
+    def seconds(self) -> int:
+        return self._seconds + self._minutes * 60 + self._houres * 3600
+
+
+def make_timecode(text: str):
+    if re.match(SecondsTimecode.PATTERN, text):
+        return SecondsTimecode(text)
+    if re.match(FormattedTimecode.PATTERN, text):
+        return FormattedTimecode(text)
+    raise InvalidTimecodeError
