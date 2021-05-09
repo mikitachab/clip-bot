50ab17893828004bcbffa1cb3f76d749ecfea6ed
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2021-05-09 17:54:17 +0200

    use munchify insted of just Munch

--- bot/text.py
+++ bot/text.py
@@ -1,37 +1,27 @@
-from munch import Munch
+from munch import munchify
 
-text = Munch(
+text = munchify(
     {
-        "url": Munch(
-            {
-                "question": "Please send video url",
-            }
-        ),
+        "url": {
+            "question": "Please send video url",
+        },
         "cancelled": "Cancelled.",
-        "start": Munch(
-            {
-                "question": "Please select time the clip should start at",
-                "choice": Munch(
-                    {
-                        "provide_timecode": "you chose to provide timecode",
-                        "url_timecode": "you chose to use timecode embedded in url",
-                        "from_start": "you chose to clip from start",
-                    }
-                ),
-                "timecode": Munch(
-                    {
-                        "question": "Please provide timecode in seconds",
-                        "invalid": "Timecode should have valid format\nPlease provide timecode (e.g. 5m20s)",
-                    }
-                ),
-            }
-        ),
-        "duration": Munch(
-            {
-                "question": "How long the clip should be in seconds?",
-                "invalid": "Duration should be a number.\nHow long the clip should be in seconds? (digits only)",
-            }
-        ),
+        "start": {
+            "question": "Please select time the clip should start at",
+            "choice": {
+                "provide_timecode": "you chose to provide timecode",
+                "url_timecode": "you chose to use timecode embedded in url",
+                "from_start": "you chose to clip from start",
+            },
+            "timecode": {
+                "question": "Please provide timecode in seconds",
+                "invalid": "Timecode should have valid format\nPlease provide timecode (e.g. 5m20s)",
+            },
+        },
+        "duration": {
+            "question": "How long the clip should be in seconds?",
+            "invalid": "Duration should be a number.\nHow long the clip should be in seconds? (digits only)",
+        },
         # fmt: off
         "confirm": "URL: {url}\n"
                    "Duration: {duration} seconds\n"
